2f75915b59604dc291d3d560261faa339d6c5d15
Death <M.26A20.M@gmail.com>
2022-09-01 12:23:26 +0430

    Installed and configured Tailwind, daisyUI and Flowbite

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,22 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+   <div class="stack">
+
+      <div class="card shadow bg-primary text-primary-content">
+         <div class="card-body">
+            <h2 class="card-title">Notification 2</h2>
+            <p>You have 3 unread messages. Tap here to see.</p>
+         </div>
+      </div>
+
+   </div>
 </template>
 
 <style>
 @media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+   .about {
+      min-height: 100vh;
+      display: flex;
+      align-items: center;
+   }
 }
 </style>
