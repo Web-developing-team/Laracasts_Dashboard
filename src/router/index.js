dc1da6ba49c794b3ebb818b27f23d7ef40e05bed
Death <M.26A20.M@gmail.com>
2022-09-02 19:35:01 +0430

    Added DashboardView, Header and Sidebar and configured a route for dashboard

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,18 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+
+import DashboardView from "@/views/DashboardView.vue";
 
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
 	routes: [
 		{
 			path: "/",
-			name: "home",
-			component: HomeView,
+			redirect: "/dashboard",
+		},
+		{
+			path: "/dashboard",
+			name: "Dashboard",
+			component: DashboardView,
 		},
 		// {
 		// 	path: "/about",
---
+++ src/views/DashboardView.vue
@@ -0,0 +1,11 @@
+<template>
+   <div class="flex">
+      <the-sidebar></the-sidebar>
+      <the-header></the-header>
+   </div>
+</template>
+
+<script setup>
+import TheSidebar from '@/components/TheSidebar.vue';
+import TheHeader from '@/components/TheHeader.vue';
+</script>
